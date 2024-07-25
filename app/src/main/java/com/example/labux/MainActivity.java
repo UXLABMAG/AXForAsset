32a71aea11fb924a2a1c39f850d25dd001c58dd5
lgarin211 <lagustinus211@gmail.com>
2024-07-25 10:31:10 +0700

    push ke mimi

--- app/src/main/java/com/example/labux/MainActivity.java
+++ app/src/main/java/com/example/labux/MainActivity.java
@@ -6,9 +6,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+//    load repeter
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+//    load item_list singgel repeter
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.items_list);
+//    }
+
+//    load item_detail singgel repeter
+//    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_items_detail);
     }
 }
